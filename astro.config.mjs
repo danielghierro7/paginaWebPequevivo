e19cc07596da21207514b957220502e0f015f33e
dgabh <dgabhie3107@g.educaand.es>
2025-07-08 10:16:57 +0200

    Añadidos nuevos componentes, modificados varios archivos y eliminados archivos obsoletos

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    output: "server",  // importante para server-side rendering
     integrations: [react()],
     adapter: netlify(),
     vite: {
