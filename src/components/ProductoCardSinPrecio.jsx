e962762a5aaa9767185b7a6af9e86b8eb50fb3d3
dgabh <dgabhie3107@g.educaand.es>
2025-07-21 03:15:53 +0200

    Añadidos nuevos componentes, modificados varios archivos y eliminados archivos obsoletos

--- src/components/ProductoCardSinPrecio.jsx
+++ src/components/ProductoCardSinPrecio.jsx
@@ -5,11 +5,12 @@ export default function ProductoCard({ id, nombre, precio, descripcion, imagenes
 
     return (
         <div className="producto-card max-w-md rounded-2xl overflow-hidden shadow-2xl bg-white p-6 text-center">
-            <ProductoMediaCarousel imagenes={imagenes} videos={videos} nombre={nombre} />
+            <ProductoMediaCarousel imagenes={imagenes} videos={videos} nombre={nombre}/>
 
             <h3 className="font-extrabold text-3xl mt-6 text-gray-900">{nombre}</h3>
             <p className="text-xl text-gray-600 mt-2">{descripcion}</p>
-            <p className="text-2xl font-bold text-green-700 mt-2">¡Consulte el precio contactándonos!</p>
+            <p className="text-base font-bold text-green-700 mt-2">¡Consulte el precio contactándonos!</p>
+
             <a
                 href={`/productos/${id}`}
                 className="inline-block mt-6 px-8 py-3 bg-yellow-400 hover:bg-yellow-500 text-black font-semibold rounded-lg shadow-md transition"
