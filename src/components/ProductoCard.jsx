d606838836172fb5643e956e80a3f47fa1c37e72
Daniel <danielghierro@gmail.com>
2025-11-06 12:50:38 +0100

    Enlace a hatsapp

--- src/components/ProductoCard.jsx
+++ src/components/ProductoCard.jsx
@@ -6,7 +6,7 @@ export default function ProductoCard({ id, nombre, precio, descripcion, imagenes
     // Enlace de WhatsApp con mensaje que incluye el ID del producto
     const whatsappNumber = "34654197649"; // con el prefijo +34 para España
     const whatsappMessage = `Hola, estoy interesado en el producto con ID: ${nombre}`;
-    const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+    const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)} para comprarlo`;
 
     return (
         <div className="producto-card max-w-md rounded-2xl overflow-hidden shadow-2xl bg-white p-6 text-center">
@@ -21,7 +21,7 @@ export default function ProductoCard({ id, nombre, precio, descripcion, imagenes
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-block mt-6 px-8 py-3 bg-yellow-400 hover:bg-yellow-500 text-black font-semibold rounded-lg shadow-md transition"
-                aria-label={`Contactar por WhatsApp sobre el producto ${nombre} para comprarlo`}
+                aria-label={`Contactar por WhatsApp sobre el producto ${nombre}`}
             >
                 Ver producto
             </a>
--- src/components/ProductoCardSinPrecio.jsx
+++ src/components/ProductoCardSinPrecio.jsx
@@ -2,6 +2,9 @@ import ProductoMediaCarousel from './ProductoCaroulse.jsx';
 
 export default function ProductoCard({ id, nombre, precio, descripcion, imagenes = [], videoUrl = [] }) {
     const videos = Array.isArray(videoUrl) ? videoUrl : [];
+    const whatsappNumber = "34654197649"; // con el prefijo +34 para España
+    const whatsappMessage = `Hola, estoy interesado en el producto con ID: ${nombre} para alquilarlo`;
+    const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
 
     return (
         <div className="producto-card max-w-md rounded-2xl overflow-hidden shadow-2xl bg-white p-6 text-center">
@@ -16,7 +19,7 @@ export default function ProductoCard({ id, nombre, precio, descripcion, imagenes
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-block mt-6 px-8 py-3 bg-yellow-400 hover:bg-yellow-500 text-black font-semibold rounded-lg shadow-md transition"
-                aria-label={`Contactar por WhatsApp sobre el producto ${nombre} para alquilarlo`}
+                aria-label={`Contactar por WhatsApp sobre el producto ${nombre}`}
             >
                 Ver producto
             </a>
